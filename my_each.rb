d9f9fdab0620ee49d11ceb9c26ce81749cce9099
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-06 22:30:15 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,8 @@
-def my_each(array)
-    i = 0
-    while i < array.length
-      yield(array[i])
-      i += 1
-      end
-    array
-    end
+def my_each(arr)
+  i = 0
+  while i < arr.length
+    yield(arr[i])
+    i += 1
+  end
+  arr
 end
